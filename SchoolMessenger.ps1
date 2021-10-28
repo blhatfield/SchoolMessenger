78820e5b0f517420731f1873997242132701589b
blhatfield <93333608+blhatfield@users.noreply.github.com>
2021-10-28 10:38:26 -0400

    Add files via upload

---
+++ SchoolMessenger.ps1
@@ -0,0 +1,5 @@
+Rename-Item -Path "C:\Program Files (x86)\SchoolMessenger\config.json" -NewName "old_Config.json"
+Invoke-WebRequest "https://github.com/blhatfield/SchoolMessenger/config_Employee_ddcc.json" -Outfile 'C:\Program Files (x86)\SchoolMessenger\config.json'
+Remove-Item -Force -Path "C:\Program Files (x86)\SchoolMessenger\icons\banner.png"
+Invoke-WebRequest "https://github.com/blhatfield/SchoolMessenger/banner.png" -Outfile 'C:\Program Files (x86)\SchoolMessenger\icons\banner.png'
+ping -n 3 127.0.0.1
