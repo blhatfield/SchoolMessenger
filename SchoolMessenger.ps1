ae6cf42b07779343d87c274c51ea7e7585736498
blhatfield <93333608+blhatfield@users.noreply.github.com>
2021-10-28 14:37:26 -0400

    Update SchoolMessenger.ps1

--- SchoolMessenger.ps1
+++ SchoolMessenger.ps1
@@ -1,7 +1,5 @@
-Stop-Process -Name messenger
 Rename-Item -Path "C:\Program Files (x86)\SchoolMessenger\config.json" -NewName "old_Config.json"
 Invoke-WebRequest "https://raw.githubusercontent.com/blhatfield/SchoolMessenger/main/config_Employee%20_ddcc.json" -Outfile 'C:\Program Files (x86)\SchoolMessenger\config.json'
 Rename-Item -Path "C:\Program Files (x86)\SchoolMessenger\icons\banner.PNG" -NewName "old_banner.png"
 Invoke-WebRequest "https://raw.githubusercontent.com/blhatfield/SchoolMessenger/main/banner.png" -Outfile 'C:\Program Files (x86)\SchoolMessenger\icons\banner.png'
-Start-Process -FilePath "C:\Program Files (x86)\SchoolMessenger\messenger.exe"
 ping -n 3 127.0.0.1
